5042a94e79713845b5fa3194f11a3bec290ee44f
A00425164 <40704271+A00425164@users.noreply.github.com>
2019-05-22 12:20:29 -0400

    Enemy Idle Patrol
    (Caution: Uses NavMeshAgent and the terrain is also baked to make it work!)
    In order to use this, create empty game objects and add the waypoints script to it to create patrol points. For the patrol to function, implement the NPCSimplePatrol script on the NPC.

---
+++ FrameWorks/Assets/Waypoints.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+	[SerializeField]
+	protected float debugDrawRadius = 1.0f;
+
+	public virtual void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position,debugDrawRadius);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
