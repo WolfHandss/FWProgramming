5c148888f75ab4586be41d0d31baf3ec2b93b852
TorontoFilm <48457924+TorontoFilm@users.noreply.github.com>
2019-06-05 11:38:05 -0400

    Dashing_Version1

--- FrameWorks/Assets/script/PlayerController.cs
+++ FrameWorks/Assets/script/PlayerController.cs
@@ -7,8 +7,12 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     public float jumpHeight = 1f;
+    public float dashDistance;
 
     public bool isGrounded;
+
+    public ParticleSystem psLeft; // Paticle system when dash left
+    public ParticleSystem psRight; // Particle System when dash right
 
     Rigidbody rb;
 
@@ -16,13 +20,17 @@ public class PlayerController : MonoBehaviour
     {
         isGrounded = true;
         rb = GetComponent<Rigidbody>();
+        psLeft.Pause();
+        psRight.Pause();
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
+        Dash();
 
+        // Jump
         if (Input.GetKey(KeyCode.Space) && isGrounded == true)
         {
             rb.AddForce(0, jumpHeight, 0, ForceMode.Impulse);
@@ -41,5 +49,25 @@ public class PlayerController : MonoBehaviour
     void OnCollisionEnter()
     {
         isGrounded = true;
+    }
+
+    void Dash()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.A) && Input.GetKey(KeyCode.LeftShift)))
+        {
+            //transform.position = Vector3.Lerp(transform.position, Vector3.left * 10, 2.0f);
+
+            transform.Translate(Vector3.Lerp(transform.position, Vector3.left * dashDistance, 2.0f), Space.Self);
+            psLeft.Play();
+            //rb.AddForce(Vector3.right * 20, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.D))
+        {
+            //transform.position = Vector3.Lerp(transform.position, Vector3.left * 10, 2.0f);
+
+            transform.Translate(Vector3.Lerp(transform.position, Vector3.right * dashDistance, 2.0f), Space.Self);
+            psRight.Play();
+            //rb.AddForce(Vector3.right * 20, ForceMode.Impulse);
+        }
     }
 }
